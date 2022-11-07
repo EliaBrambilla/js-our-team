20dd77fd3b08f1e873fc9e86a39da8ee2799b3a9
EliaBrambilla <elia.brambilla@icloud.com>
2022-11-07 10:30:04 +0100

    aggiungo persone disponibili js

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+//aggiungo persone disponibili
+const team = [
+    {
+      name: "Wayne Barnett",
+      role: "Founder & CEO",
+      photo: "img/wayne-barnett-founder-ceo.jpg",
+      qualities: ['Great Team Leader',
+      'Always Untiring'
+      ]
+    },
+    {
+      name: "Angela Caroll",
+      role: "Chief Editor",
+      photo: "img/angela-caroll-chief-editor.jpg",
+      qualities: ['Unconventional Girl']
+    },
+    {
+      name: "Walter Gordon",
+      role: "Office Manager",
+      photo: "img/walter-gordon-office-manager.jpg",
+      qualities: ['With clear Ideas']
+    },
+    {
+      name: "Angela Lopez",
+      role: "Social Media Manager",
+      photo: "img/angela-lopez-social-media-manager.jpg",
+      qualities: ['Little Angie']
+    },
+    {
+      name: "Scott Estrada",
+      role: "Developer",
+      photo: "img/scott-estrada-developer.jpg",
+      qualities: ['Problem Solver']
+    },
+    {
+      name: "Barbara Ramos",
+      role: "Graphic Designer",
+      photo: "img/barbara-ramos-graphic-designer.jpg",
+      qualities: ['Always Smiling']
+    }
+  ];
+
+  
+  
